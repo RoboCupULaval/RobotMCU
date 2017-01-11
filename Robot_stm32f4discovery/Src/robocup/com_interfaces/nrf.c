30d316fe9c4cd8953785b7062f88d0b217d53777
Frédéric St-Pierre <frederic.st-pierre.7@ulaval.ca>
2017-01-11 16:09:36 -0500

    added files

---
+++ Robot_stm32f4discovery/Src/robocup/com_interfaces/nrf.c
@@ -0,0 +1,32 @@
+#include "nrf.h"
+#include "../nrfDriver/nrfDriver.h"
+
+comHandle_t nrf_init(void){
+	nrfInit();
+
+	comHandle_t com;
+	com.read = nrf_read;
+	com.write = nrf_write;
+	com.readUntilZero = nrf_readUntilZero;
+	return com;
+}
+
+
+size_t nrf_read(__attribute__ ((unused)) void *pBuffer,
+		        __attribute__ ((unused)) size_t expectedLength){
+	// no need right now
+	return 0;
+}
+
+size_t nrf_write(const void *pBuffer, size_t length){
+	nrfSend(pBuffer);
+	return length;
+}
+
+size_t nrf_readUntilZero(void *pBuffer,
+		                 __attribute__ ((unused)) size_t maxLength){
+
+	// TODO modify nrfReceive to take maxLength into account
+	nrfReceive(pBuffer);
+	return strlen(pBuffer);
+}
