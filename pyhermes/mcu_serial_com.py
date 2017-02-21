24c859b7f72bba39210101d356178fee6e12f518
Léonard Gagnon <leonard.gagnon@gmail.com>
2017-02-20 21:45:12 -0500

    Adding usbmodem for macos basestation

--- pyhermes/mcu_serial_com.py
+++ pyhermes/mcu_serial_com.py
@@ -19,6 +19,7 @@ def getFirstSerialPort():
     ttyList +=  glob.glob('/dev/rfcomm*')
     ttyList +=  glob.glob('/dev/ttyUSB*')
     ttyList +=  glob.glob('/dev/tty.Robot*')
+    ttyList +=  glob.glob('/dev/tty.usbmodem*')
 
     if len(ttyList) == 0:
         print('No serial device found! Exiting...')
