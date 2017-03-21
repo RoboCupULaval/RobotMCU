b68a66c706ffd3312ad2bbac68d505f6c93543d0
Philippe Babin <philippe.babin@gmail.com>
2017-03-20 20:05:49 -0400

    Forgot log.h

---
+++ Robot_stm32f4discovery/Src/robocup/log.h
@@ -0,0 +1,25 @@
+/*
+ * log.h
+ *
+ *  Created on: Mar 20, 2017
+ *      Author: philippe
+ */
+
+#ifndef ROBOCUP_LOG_H_
+#define ROBOCUP_LOG_H_
+
+#include "com_interfaces/com_interface.h"
+
+#define MAX_METADATA_LEN 128
+
+void log_init(comHandle_t comInterface);
+void log_setBatteryVoltage(const double batteryVoltage);
+void LOG_INFO(const char * pMessage);
+void LOG_ERROR(const char * pMessage);
+void LOG_DEBUG(const char * pMessage);
+void LOG_DATA(const char * pMessage);
+void LOG_INFO_AND_BUFFER(const char * pMessage, void * pBuffer, size_t length);
+void LOG_ERROR_AND_BUFFER(const char * pMessage, void * pBuffer, size_t length);
+void LOG_DEBUG_AND_BUFFER(const char * pMessage, void * pBuffer, size_t length);
+
+#endif /* ROBOCUP_LOG_H_ */
