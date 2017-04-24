01fe1f85dd4be6a2c5cfbe8fffb8e96e1adf194a
Frédéric St-Pierre <frederic.st-pierre.7@ulaval.ca>
2017-04-24 01:27:24 -0400

    moved the per-packet structs to packet_definitions.h since this info is useful for the comm. tower too (packet size among else)

--- Robot_stm32f4discovery/Src/robocup/hermes/commands.h
+++ Robot_stm32f4discovery/Src/robocup/hermes/commands.h
@@ -10,45 +10,6 @@
 
 #include "stdint.h"
 
-/**** DEFINITION OF COMMAND MESSAGE STRUCT ****/
-
-typedef struct __attribute__((__packed__)) {
-	packetHeaderStruct_t header;
-} msg_no_arg_t;
-
-typedef struct __attribute__((__packed__)) {
-	packetHeaderStruct_t header;
-	float   vx;
-	float   vy;
-	float   vtheta;
-} msg_set_speed_t;
-
-typedef struct __attribute__((__packed__)) {
-	packetHeaderStruct_t header;
-	float   cmd1; //pwm to wheel 1 between 0.0 and 1.0
-	float   cmd2;
-	float   cmd3;
-	float	cmd4;
-} msg_set_speed_open_t;
-
-typedef struct __attribute__((__packed__)) {
-	packetHeaderStruct_t header;
-	float   kp;
-	float   ki;
-	float   kd;
-} msg_set_pid_t;
-
-typedef struct __attribute__((__packed__)) {
-	packetHeaderStruct_t header;
-	uint8_t registe;
-	uint8_t value;
-} msg_set_register_t;
-
-typedef struct __attribute__((__packed__)) {
-	packetHeaderStruct_t header;
-	uint8_t value;
-} msg_uint8_t;
-
 enum registerTypes_t {
 	CONTROL_LOOP_STATE = 0,
 	KICK_COMMAND = 1,
--- Robot_stm32f4discovery/Src/robocup/hermes/packets_definitions.h
+++ Robot_stm32f4discovery/Src/robocup/hermes/packets_definitions.h
@@ -20,6 +20,48 @@ typedef struct __attribute__((__packed__)) {
 	uint8_t checksum;
 } packetHeaderStruct_t;
 
+
+/**** DEFINITION OF COMMAND MESSAGE STRUCT ****/
+
+typedef struct __attribute__((__packed__)) {
+	packetHeaderStruct_t header;
+} msg_no_arg_t;
+
+typedef struct __attribute__((__packed__)) {
+	packetHeaderStruct_t header;
+	float   vx;
+	float   vy;
+	float   vtheta;
+} msg_set_speed_t;
+
+typedef struct __attribute__((__packed__)) {
+	packetHeaderStruct_t header;
+	float   cmd1; //pwm to wheel 1 between 0.0 and 1.0
+	float   cmd2;
+	float   cmd3;
+	float	cmd4;
+} msg_set_speed_open_t;
+
+typedef struct __attribute__((__packed__)) {
+	packetHeaderStruct_t header;
+	float   kp;
+	float   ki;
+	float   kd;
+} msg_set_pid_t;
+
+typedef struct __attribute__((__packed__)) {
+	packetHeaderStruct_t header;
+	uint8_t register;
+	uint8_t value;
+} msg_set_register_t;
+
+typedef struct __attribute__((__packed__)) {
+	packetHeaderStruct_t header;
+	uint8_t value;
+} msg_uint8_t;
+
+
+
 // This defines the ID number for each packet.
 enum packetTypes_t {
 	PING_REQUEST = 0,
