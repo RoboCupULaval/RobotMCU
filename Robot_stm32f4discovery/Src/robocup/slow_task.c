249c253885b1eab0008c05127a3e2e14531a926b
Unknown <francis.lachapelle.1@ulaval.ca>
2018-06-06 20:06:38 -0400

    Power indicators updated

--- Robot_stm32f4discovery/Src/robocup/slow_task.c
+++ Robot_stm32f4discovery/Src/robocup/slow_task.c
@@ -4,7 +4,8 @@
 
 powerState slow_handleBattProtection(void);
 void slow_updatePowerIndicators(void);
-slow_turnOnLeds(uint8_t number_of_leds_on);
+void slow_updatePowerWarning(uint16_t number_of_iterations);
+void slow_turnOnLeds(uint8_t number_of_leds_on);
 void slow_turnOffLeds(uint8_t first_led_off);
 
 void slow_taskEntryPoint(void) {
@@ -22,7 +23,7 @@ void slow_taskEntryPoint(void) {
 
 	uint8_t id = robot_getPlayerID();
 	for(;;) {
-		//Debug modee
+		//Debug mode
 		if(robot_isDebug()) {
 			led_turnOn(8);
 
@@ -54,41 +55,73 @@ powerState slow_handleBattProtection(void) {
 	switch(state) {
 	case POWER_CRITICAL:
 		pmu_disablePower();
+		slow_updatePowerWarning(SLOW_CRITICAL_LED_FLASH);
 		break;
 	case POWER_WARNING:
-		slow_turnOnLeds(1);
-		slow_turnOffLeds(2);
+		slow_updatePowerWarning(SLOW_WARNING_LED_FLASH);
 		break;
 	case POWER_OK:
 		slow_updatePowerIndicators();
 		pmu_forceEnablePower();
 		break;
+	case POWER_OVERRIDE:
+	default:
+		while(1);
 	};
 
 	return state;
 }
 
 void slow_updatePowerIndicators(void) {
-	double battery_voltage 			   = pmu_getBattVoltage();
-	double battery_voltage_no_offset   = battery_voltage - PMU_BATT_WARNING_TRESHOLD;
-	double battery_increment 		   = battery_voltage_no_offset / SLOW_TOTAL_NUMBER_OF_LEDS;
+	static uint8_t last_nb_leds_on     = 100;
+	static uint16_t nb_iterations_hyst = 0;
+	double batt_voltage 			   = pmu_getBattVoltage();
+	double batt_voltage_max_no_offset  = SLOW_BATT_MAX - PMU_BATT_WARNING_TRESHOLD;
+	double batt_voltage_no_offset      = batt_voltage - PMU_BATT_WARNING_TRESHOLD;
+	double batt_increment 		   	   = batt_voltage_max_no_offset / SLOW_TOTAL_NUMBER_OF_LEDS;
 
-	uint8_t first_led_off 		       = battery_voltage_no_offset / battery_increment;
-	uint8_t number_of_leds_on          = first_led_off - 1;
+	uint8_t nb_leds_on   			   = (uint8_t)(batt_voltage_no_offset / batt_increment);
+	uint8_t first_led_off          	   = (uint8_t)(nb_leds_on + 1);
+	if (nb_leds_on == 0)
+		slow_updatePowerWarning(SLOW_ZERO_LED_FLASH);
+	else if (last_nb_leds_on != nb_leds_on) {
+		if ((nb_iterations_hyst < SLOW_HYSTERESIS_ITERATIONS) && last_nb_leds_on != 100) {
+			++nb_iterations_hyst;
+		} else {
+			slow_turnOnLeds(nb_leds_on);
+			slow_turnOffLeds(first_led_off);
+			nb_iterations_hyst = 0;
+		}
+	}
+	last_nb_leds_on = nb_leds_on;
+}
 
-	slow_turnOnLeds(number_of_leds_on);
-	slow_turnOffLeds(first_led_off);
+void slow_updatePowerWarning(uint16_t number_of_iterations) {
+	static uint32_t iterations_count = 0;
+	static bool led_is_open = false;
+	if (iterations_count < number_of_iterations)
+		++iterations_count;
+	else {
+		if (led_is_open) {
+			led_turnOff(0);
+			led_is_open = false;
+		} else {
+			led_turnOn(0);
+			led_is_open = true;
+		}
+		iterations_count = 0;
+	}
 }
 
 void slow_turnOnLeds(uint8_t number_of_leds_on) {
-	for (int i = 1; i <= number_of_leds; ++i) {
+	for (uint8_t i = 0; i <= number_of_leds_on; ++i) {
 		led_turnOn(i);
 	}
 }
 
 // first_led_off: the first led that is off
 void slow_turnOffLeds(uint8_t first_led_off) {
-	for (int i = first_led_off; i <= SLOW_TOTAL_NUMBER_OF_LEDS; ++i) {
+	for (uint8_t i = first_led_off; i <= SLOW_TOTAL_NUMBER_OF_LEDS; ++i) {
 		led_turnOff(i);
 	}
 }
--- Robot_stm32f4discovery/Src/robocup/slow_task.h
+++ Robot_stm32f4discovery/Src/robocup/slow_task.h
@@ -8,8 +8,12 @@
 #include "kicker.h"
 #include "ball_detector.h"
 
-#define 	SLOW_BATT_MAX					16.8//V
+#define 	SLOW_BATT_MAX					16//V
 #define		SLOW_TOTAL_NUMBER_OF_LEDS		9
+#define		SLOW_WARNING_LED_FLASH			20
+#define		SLOW_CRITICAL_LED_FLASH			10
+#define		SLOW_ZERO_LED_FLASH				40
+#define 	SLOW_HYSTERESIS_ITERATIONS		40
 
 void slow_taskEntryPoint(void);
 
