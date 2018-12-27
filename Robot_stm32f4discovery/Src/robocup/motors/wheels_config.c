66e1e2ecb1756d77976837768f90dfd8926bcdb0
philippe <philippe.lebel008@sympatico.ca>
2018-12-27 11:42:01 -0500

    modif param wheels et mnrc

--- Robot_stm32f4discovery/Src/robocup/motors/wheels_config.c
+++ Robot_stm32f4discovery/Src/robocup/motors/wheels_config.c
@@ -10,10 +10,10 @@ Wheel_t g_wheels[] =
 {// diameter 66 mm
 #ifdef DELTA
 #define RADIUS 0.033f // m
-#define TICK_PER_TURN 5.f * 4096.0f // tick/turn
-#define DIST_FROM_CENTER 0.0785f // m
+#define TICK_PER_TURN 3.571428571428572f * 4096.0f // tick/turn
+#define DIST_FROM_CENTER 0.08051f // m
 		// angle of speed vector 60, 141
-		// Distance from center to wheel  78.5 mm
+		// Distance from center to wheel  80,51 mm
 {"wheel 1", {0}, EncoderTim1, &htim5, TIM_CHANNEL_1, MOTOR1_DIR_GPIO_Port, MOTOR1_DIR_Pin, ClockWise, (float)(+60.f / 180.0f * M_PI),  1.0f, cosf((60.f + 90.f) / 180.0f * M_PI),   sinf((60.f + 90.f) / 180.0f * M_PI),   RADIUS, DIST_FROM_CENTER, TICK_PER_TURN, 0.0f},
 {"wheel 2", {0}, EncoderTim2, &htim8, TIM_CHANNEL_3, MOTOR2_DIR_GPIO_Port, MOTOR2_DIR_Pin, ClockWise, (float)(+141.f/ 180.0f * M_PI), 1.0f, cosf((141.f + 90.f) / 180.0f * M_PI),  sinf((141.f + 90.f) / 180.0f * M_PI),  RADIUS, DIST_FROM_CENTER, TICK_PER_TURN, 0.0f},
 {"wheel 3", {0}, EncoderTim3, &htim5, TIM_CHANNEL_3, MOTOR3_DIR_GPIO_Port, MOTOR3_DIR_Pin, ClockWise, (float)(-141.f/ 180.0f * M_PI),  1.0f, cosf((-141.f + 90.f) / 180.0f * M_PI), sinf((-141.f + 90.f) / 180.0f * M_PI),  RADIUS, DIST_FROM_CENTER, TICK_PER_TURN, 0.0f},
