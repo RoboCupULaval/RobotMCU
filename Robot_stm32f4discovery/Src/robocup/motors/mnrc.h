1d022fc87ce5e662c96c5816e8f8a6bd9e9c6097
Philippe Babin <philippe.babin@gmail.com>
2019-06-10 17:21:23 -0400

    Merge remove_deadzone and dev

--- Robot_stm32f4discovery/Src/robocup/motors/mnrc.h
+++ Robot_stm32f4discovery/Src/robocup/motors/mnrc.h
@@ -19,6 +19,7 @@ typedef struct {
 	// the MNRC parameters
 	float Kp;    // proportionnal gain
 	float Ki;    // integral gain
+	float Kd;	// derivative gain
 	float lambda; // desired closed-loop dynamic
 
 	float K1;
@@ -31,6 +32,7 @@ typedef struct {
 	float w_m[4];    // Desired dynamic estimation [rad/s]
 	float w_ref[4];  // Wheel reference [rad/s]
 	float e[4];      // Error between desired dynamic and measured wheel speed
+	float eD[4];	// Derivative of e
 	float eI[4];     // Integration of e
 	float anti_windup[4][ANTI_WINDUP_CIRCULAR_BUFFER_SIZE]; // Previous e value in a circular buffer
 	size_t head_anti_windup[4]; // current e value in circular buffer of anti windup
@@ -39,7 +41,7 @@ typedef struct {
 
 } MNRC_t;
 
-MNRC_t MNRC_init(float Kp, float Ki, float gamma);
+MNRC_t MNRC_init(float Kp, float Ki, float Kd, float gamma);
 void MNRC_update(MNRC_t *mnrc);
 void MNRC_reset(MNRC_t *mnrc);
 
